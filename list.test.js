62fa10f8ec2efc1effade9110c69f60313c08ee3
Dmitry Rocha <dmitryrck@gmail.com>
2018-09-28 14:58:38 +1200

    Add delete and update test.

--- list.test.js
+++ list.test.js
@@ -3,20 +3,24 @@ import { Selector } from 'testcafe'
 fixture `List`
   .page `https://dry-ocean-41976.herokuapp.com/`
   .before(async ctx => {
-    ctx.number = parseInt(Math.random() * 1000)
+    ctx.number = function () {
+      return parseInt(Math.random() * 1000)
+    }
   })
 
 test("should be able to create a new list", async t => {
+  const number = t.fixtureCtx.number()
   await t
-    .typeText("#list_name", `List${t.fixtureCtx.number}`)
+    .typeText("#list_name", `List${number}`)
     .click("button")
     .expect(Selector("h1.mt-5").innerText)
-    .eql(`List: List${t.fixtureCtx.number}`)
+    .eql(`List: List${number}`)
 })
 
 test("should add a new user", async t => {
+  const number = t.fixtureCtx.number()
   await t
-    .typeText("#list_name", `List${t.fixtureCtx.number}`)
+    .typeText("#list_name", `List${number}`)
     .click("button")
     .click(Selector("a.btn-info"))
     .typeText("#name", "John Doe")
@@ -25,4 +29,38 @@ test("should add a new user", async t => {
     .click(".btn-primary")
     .expect(Selector("body").innerText)
     .contains("John Doe")
+})
+
+test("should be able to delete", async t => {
+  const number = t.fixtureCtx.number()
+  await t
+    .typeText("#list_name", `List${number}`)
+    .click("button")
+    .click(Selector("a.btn-info"))
+    .typeText("#name", "John Doe")
+    .typeText("#email", "john.doe@example.com")
+    .typeText("#password", "secret")
+    .click(".btn-primary")
+    .click("[title='Delete']")
+    .expect(Selector("body").innerText)
+    .notContains("John Doe")
+})
+
+test("should be able to update", async t => {
+  const number = t.fixtureCtx.number()
+  await t
+    .typeText("#list_name", `List${number}`)
+    .click("button")
+    .click(Selector("a.btn-info"))
+    .typeText("#name", "John Doe")
+    .typeText("#email", "john.doe@example.com")
+    .typeText("#password", "secret")
+    .click(".btn-primary")
+    .click("[title='Edit']")
+    .typeText("#name", "Alice Doe")
+    .typeText("#email", "alice.doe@example.com")
+    .typeText("#password", "secret")
+    .click(".btn-primary")
+    .expect(Selector("body").innerText)
+    .contains("Alice Doe")
 })
