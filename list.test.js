dcba4957ec5e3c4bb06a598a1478b2eb6ce993d5
Dmitry Rocha <dmitryrck@gmail.com>
2018-09-28 12:28:21 +1200

    Use a random number to create lists.

--- list.test.js
+++ list.test.js
@@ -2,18 +2,21 @@ import { Selector } from 'testcafe'
 
 fixture `List`
   .page `https://dry-ocean-41976.herokuapp.com/`
+  .before(async ctx => {
+    ctx.number = parseInt(Math.random() * 1000)
+  })
 
 test("should be able to create a new list", async t => {
   await t
-    .typeText("#list_name", "List1")
+    .typeText("#list_name", `List${t.fixtureCtx.number}`)
     .click("button")
     .expect(Selector("h1.mt-5").innerText)
-    .eql("List: List1")
+    .eql(`List: List${t.fixtureCtx.number}`)
 })
 
 test("should add a new user", async t => {
   await t
-    .typeText("#list_name", "List1")
+    .typeText("#list_name", `List${t.fixtureCtx.number}`)
     .click("button")
     .click(Selector("a.btn-info"))
     .typeText("#name", "John Doe")
